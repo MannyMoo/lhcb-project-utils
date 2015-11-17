1b9a44f59ef7b0a7c75e4b9ebea139b0679deeb8
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2015-11-17 15:36:44 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Utilities for configuring your linux environment for doing PPE projects, assuming you're running on a Glasgow desktop or ppelx.
---
+++ setup-root.sh
@@ -0,0 +1,43 @@
+
+function _setup_root() {
+    local CMTCONFIG="$1"
+    local GCCVERSION="$2"
+    local PYTHONVERSION="$3"
+    local ROOTVERSION="$4"
+    . /afs/cern.ch/sw/lcg/external/gcc/${GCCVERSION}/${CMTCONFIG}/setup.sh
+    export PATH="/afs/cern.ch/sw/lcg/external/Python/${PYTHONVERSION}/${CMTCONFIG}/bin:${PATH}"
+    export LD_LIBRARY_PATH="/afs/cern.ch/sw/lcg/external/Python/${PYTHONVERSION}/${CMTCONFIG}/lib:${LD_LIBRARY_PATH}"
+    cd "/afs/cern.ch/sw/lcg/app/releases/ROOT/${ROOTVERSION}/${CMTCONFIG}/root/"
+    . ./bin/thisroot.sh;
+    cd -
+}
+
+function setup_root_slc5() {
+    CMTCONFIG=x86_64-slc5-gcc47-opt
+    GCCVERSION=4.7.2p1
+    PYTHONVERSION=2.7.3
+    ROOTVERSION=5.34.26
+    _setup_root $CMTCONFIG $GCCVERSION $PYTHONVERSION $ROOTVERSION
+}
+
+function setup_root_slc6() {
+    # From SetupProject LCGCMT 72a ROOT pytools (latest version that doesn't use ROOT 6.X)
+    CMTCONFIG=x86_64-slc6-gcc48-opt
+    GCCVERSION=4.8.1
+    PYTHONVERSION=2.7.6
+    ROOTVERSION=5.34.25
+    _setup_root $CMTCONFIG $GCCVERSION $PYTHONVERSION $ROOTVERSION
+}
+
+function setup_root() {
+    local RELEASE="`cat /etc/redhat-release`"
+    if [ `expr match "$RELEASE" ".*5\."` -ne 0 ] ; then
+	setup_root_slc5
+    else
+	setup_root_slc6
+    fi
+}
+
+function setup_lhcb_env() {
+    source /afs/cern.ch/lhcb/software/releases/LBSCRIPTS/prod/InstallArea/scripts/LbLogin.sh
+}
